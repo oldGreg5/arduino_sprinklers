ef012f79575adccdebeb6e3b045e16063d5b969e
ejajanu <janusz.janus@ericsson.com>
2016-05-25 16:32:03 +0200

    first draft

--- arduino_sprinklers/arduino_sprinklers.ino
+++ arduino_sprinklers/arduino_sprinklers.ino
@@ -1,12 +1,22 @@
 #include "Arduino.h"
-//The setup function is called once at startup of the sketch
-void setup()
-{
+//define pins for hydro valves relays
+const int RELAYS_PINS[] = { 3, 4, 5, 6 };
+const int WELL_PUMP_PIN = 7;
+const int TANK_FULL_PIN = 8;
+int LED = 13; // Use the onboard Uno LED
+
+void setup() {
+	Serial.begin(9600);
+	pinMode(TANK_FULL_PIN, INPUT);
+
 // Add your initialization code here
 }
 
 // The loop function is called in an endless loop
-void loop()
-{
-//Add your repeated code here
+void loop() {
+	//read tank moisture sensor
+	int tankFull = digitalRead(TANK_FULL_PIN);
+	Serial.println("Tank full: " + tankFull);
+
+	delay(5);
 }
