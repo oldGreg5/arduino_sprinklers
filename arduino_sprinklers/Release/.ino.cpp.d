276faf7e8d6808e822bb2f2a6a36dc68cf5f16b5
ejajanu <janusz.janus@ericsson.com>
2016-05-25 15:21:55 +0200

    setup

---
+++ arduino_sprinklers/Release/.ino.cpp.d
@@ -0,0 +1,40 @@
+.ino.cpp.o: ../.ino.cpp \
+ C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/Arduino.h \
+ C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/binary.h \
+ C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/WCharacter.h \
+ C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/WString.h \
+ C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/HardwareSerial.h \
+ C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/Stream.h \
+ C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/Print.h \
+ C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/Printable.h \
+ C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/USBAPI.h \
+ C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/Arduino.h \
+ C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\variants\eightanaloginputs/pins_arduino.h \
+ C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\variants\eightanaloginputs/../standard/pins_arduino.h \
+ ../arduino_sprinklers.ino
+
+C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/Arduino.h:
+
+C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/binary.h:
+
+C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/WCharacter.h:
+
+C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/WString.h:
+
+C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/HardwareSerial.h:
+
+C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/Stream.h:
+
+C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/Print.h:
+
+C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/Printable.h:
+
+C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/USBAPI.h:
+
+C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino/Arduino.h:
+
+C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\variants\eightanaloginputs/pins_arduino.h:
+
+C:\Users\janu\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\variants\eightanaloginputs/../standard/pins_arduino.h:
+
+../arduino_sprinklers.ino:
