09dec0cf491408dd78b9c7e4c49e47cbc04dc2cf
ejajanu <janusz.janus@ericsson.com>
2016-06-15 12:24:44 +0200

    Working version 1
    Change-Id: Ic8fc5ad69c8e5983db6d63f6c0b43eb5fd42726f
    Signed-off-by: ejajanu <janusz.janus@ericsson.com>

--- arduino_sprinklers/arduino_sprinklers.ino
+++ arduino_sprinklers/arduino_sprinklers.ino
@@ -1,22 +1,336 @@
-#include "Arduino.h"
-//define pins for hydro valves relays
-const int RELAYS_PINS[] = { 3, 4, 5, 6 };
-const int WELL_PUMP_PIN = 7;
-const int TANK_FULL_PIN = 8;
-int LED = 13; // Use the onboard Uno LED
+//#define BLYNK_DEBUG
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <ESP8266_Lib.h>
+#include <BlynkSimpleShieldEsp8266.h>
+#include <SimpleTimer.h>
 
-void setup() {
-	Serial.begin(9600);
-	pinMode(TANK_FULL_PIN, INPUT);
+// Set ESP8266 Serial object
+#define EspSerial Serial
+#define PIN_WATER_SENSOR 0
+#define PIN_WELL_PUMP 11
+#define VPIN_HYDRO 5
+#define PIN_HYDRO 10
+#define VPIN_SLIDER_RESET 2
+#define VPIN_SLIDER_TIME 0
+#define VPIN_START 1
+#define VPIN_S1 6
+#define VPIN_S2 7
+#define VPIN_S3 8
+#define VPIN_S4 9
+#define PIN_S1 4
+#define PIN_S2 5
+#define PIN_S3 6
+#define PIN_S4 7
 
-// Add your initialization code here
+ESP8266 wifi(&EspSerial);
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "77a8fc1fbd2046299004d28217bc4199";
+
+SimpleTimer timer;
+//SimpleTimer timerWell;
+
+int sensor_water = A0;
+int value_water;
+//default watering time
+int wateringTimeSliderValueDefault = 20;
+int wateringTimeSliderValue;
+int resetSliderValue;
+boolean tankFull = false;
+//0 cold star,1 START ,2 PAUSE
+short routineState = 0;
+short startButton = 0;
+//short sprinklers = 4;
+int sprinklersStatus[4];
+int sprinklers[4];
+int finishedSprinklers = 0;
+
+WidgetLCD lcd(V3);
+
+BLYNK_WRITE(VPIN_SLIDER_TIME)
+{
+	//   BLYNK_LOG("Got a value: %s", param.asStr());
+	wateringTimeSliderValue = param.asInt();
+	//analogWrite(1,wateringTimeSliderValue);
+	//  Blynk.virtualWrite(V1, wateringTimeSliderValue); //show slider value on vDisplay with Push setting
 }
 
-// The loop function is called in an endless loop
-void loop() {
-	//read tank moisture sensor
-	int tankFull = digitalRead(TANK_FULL_PIN);
-	Serial.println("Tank full: " + tankFull);
+BLYNK_WRITE(VPIN_START)
+{
+	//   BLYNK_LOG("Got a value: %s", param.asStr());
+	startButton = param.asInt();
+	//  wateringTimeSliderValue = wateringTimeSliderValueDefault;
+	if (startButton == 1) {
+		//start routine
+		//if (routineState == 0 || routineState == 2) {
+		routineState = 1;
+		digitalWrite(PIN_HYDRO , 0);
+		wateringRoutine();
+		//}
+	} else {
+		routineState = 2;
+		digitalWrite(PIN_HYDRO , 1);
+		for (int j = 0; j < 4; j++) {
+			digitalWrite(sprinklers[j] , 1);
+		}
+	}
+	//analogWrite(1,wateringTimeSliderValue);
+	//  Blynk.virtualWrite(V1, startButton); //show slider value on vDisplay with Push setting
+}
 
-	delay(5);
+BLYNK_WRITE(VPIN_SLIDER_RESET)
+{
+	//when set to 100 reset routine to initial values
+	resetSliderValue = param.asInt();
+	//analogWrite(1,wateringTimeSliderValue);
+	if (resetSliderValue == 100) {
+		resetWateringRoutine();
+	}
+	//  Blynk.virtualWrite(V1, resetSliderValue); //show slider value on vDisplay with Push setting
+}
+
+BLYNK_WRITE(VPIN_HYDRO)
+{
+	//  resetSliderValue = param.asInt();
+	//analogWrite(1,wateringTimeSliderValue);
+	if (param.asInt() == 1) {
+		digitalWrite(PIN_HYDRO, 0);
+	} else {
+		digitalWrite(PIN_HYDRO, 1);
+	}
+	//  Blynk.virtualWrite(V1, resetSliderValue); //show slider value on vDisplay with Push setting
+}
+
+BLYNK_WRITE(VPIN_S1)
+{
+	if (param.asInt() == 1) {
+		digitalWrite(PIN_S1, 0);
+	} else {
+		digitalWrite(PIN_S1, 1);
+	}
+}
+
+BLYNK_WRITE(VPIN_S2)
+{
+	if (param.asInt() == 1) {
+		digitalWrite(PIN_S2, 0);
+	} else {
+		digitalWrite(PIN_S2, 1);
+	}
+}
+
+BLYNK_WRITE(VPIN_S3)
+{
+	if (param.asInt() == 1) {
+		digitalWrite(PIN_S3, 0);
+	} else {
+		digitalWrite(PIN_S3, 1);
+	}
+}
+
+BLYNK_WRITE(VPIN_S4)
+{
+	if (param.asInt() == 1) {
+		digitalWrite(PIN_S4, 0);
+	} else {
+		digitalWrite(PIN_S4, 1);
+	}
+}
+
+//BLYNK_WRITE(VPIN_SLIDER_RESET)
+//{
+//  int reset = param.asInt();
+//  //analogWrite(1,wateringTimeSliderValue);
+//  if (reset == 1) {
+//    Blynk.virtualWrite(VPIN_SLIDER_RESET,0);
+//  }
+//  //  Blynk.virtualWrite(V1, resetSliderValue); //show slider value on vDisplay with Push setting
+//}
+
+void checkWaterTank() {
+	//  if (!tankFull) {
+	value_water = analogRead(PIN_WATER_SENSOR);
+	//tank full
+	if (value_water > 800 && tankFull == true) {
+		//    tankFull = false;
+		//    lcd.print(0, 1, "Tank full");
+		//    digitalWrite (PIN_WELL_PUMP, LOW);
+		//    timerWell.restartTimer(1);
+		//  } else {
+		tankFull = false;
+		lcd.print(0, 1, "                ");
+		lcd.print(0, 1, "Filling tank...");
+		digitalWrite(PIN_WELL_PUMP, LOW);
+		//  } else {
+		//    lcd.print(0, 1, "Tank full");
+		//    digitalWrite (PIN_WELL_PUMP, HIGH);
+		//  }
+	}
+}
+
+void resetWateringRoutine() {
+	//turn off all valves
+	Blynk.virtualWrite(VPIN_S1, 0);
+	Blynk.virtualWrite(VPIN_S2, 0);
+	Blynk.virtualWrite(VPIN_S3, 0);
+	Blynk.virtualWrite(VPIN_S4, 0);
+	Blynk.virtualWrite(VPIN_START, 0);
+	Blynk.virtualWrite(VPIN_HYDRO, 0);
+	Blynk.virtualWrite(VPIN_SLIDER_TIME, wateringTimeSliderValueDefault);
+	wateringTimeSliderValue = wateringTimeSliderValueDefault;
+	Blynk.virtualWrite(VPIN_SLIDER_RESET, 0);
+
+	//set default sprinklers time to 0 minutes
+	for (int i = 0; i < 4; i++) {
+		sprinklersStatus[i] = 0;
+		digitalWrite(sprinklers[i], 1);
+	}
+
+	digitalWrite(PIN_HYDRO, 1);
+
+	if (routineState == 2) {
+		lcd.print(0, 0, "                ");
+		lcd.print(0, 0, "Status: FINISHED");
+	} else {
+		routineState = 0;
+		//  lcd.clear(); //Use it to clear the LCD Widget
+		lcd.print(0, 0, "                ");
+		lcd.print(0, 0, "Status: RESET");
+	}
+}
+
+void wateringRoutine() {
+	//start routine
+	if (routineState == 1) {
+		for (int i = 0; i < 4; i++) {
+
+			//      lcd.print(0, 1, "                ");
+			//      String tmp1 = "*";
+			//      tmp1 += sprinklersStatus[i];
+			//      tmp1 += "/";
+			//      tmp1 += wateringTimeSliderValue;
+			//      lcd.print(0, 1, tmp1);
+			if (sprinklersStatus[i] < wateringTimeSliderValue) {
+				//        lcd.print(0, 0, "                ");
+				//        String slogan = "#" + i;
+				//        slogan += " ->" + sprinklersStatus[i];
+				//        slogan += "  " + sprinklers[i];
+				String tmp = "#";
+				tmp += i + 1;
+				tmp += " ->";
+				tmp += sprinklersStatus[i];
+				//        tmp += "  ";
+				//        tmp += sprinklers[i];
+				lcd.print(0, 0, "                ");
+				lcd.print(0, 0, tmp);
+
+				sprinklersStatus[i]++;
+				digitalWrite(sprinklers[i], 0);
+				//turn off other valves
+				for (int j = 0; j < 4; j++) {
+					if (j != i) {
+						digitalWrite(sprinklers[j], 1);
+					}
+				}
+				delay(50);
+				break;
+			}
+		}
+		//    lcd.print(6, 1, sprinklersStatus[3]);
+		if (sprinklersStatus[3] >= wateringTimeSliderValue) {
+			routineState = 2;
+			//      finishedSprinklers=0;
+			resetWateringRoutine();
+		}
+		//all sprinklers done
+		//    lcd.print(0, 0, "                ");
+		//        routineState = 0;
+		//    lcd.print(0, 1, "                ");
+		//    lcd.print(0, 0, "Status: FINISH");
+		//    resetWateringRoutine();
+	}
+}
+
+void setup() {
+	// Set console baud rate
+	Serial.begin(9600);
+	delay(10);
+	// Set ESP8266 baud rate
+	EspSerial.begin(115200);
+	delay(10);
+	Blynk.begin(auth, wifi, "babilons", "thai0lai5");
+
+	pinMode(PIN_WATER_SENSOR, INPUT);
+	pinMode(PIN_WELL_PUMP, OUTPUT);
+	pinMode(PIN_HYDRO, OUTPUT);
+	pinMode(PIN_S1, OUTPUT);
+	pinMode(PIN_S2, OUTPUT);
+	pinMode(PIN_S3, OUTPUT);
+	pinMode(PIN_S4, OUTPUT);
+
+	//set default sprinklers time to 0 minutes
+	sprinklersStatus[0] = 0;
+	sprinklersStatus[1] = 0;
+	sprinklersStatus[2] = 0;
+	sprinklersStatus[3] = 0;
+
+	sprinklers[0] = PIN_S1;
+	sprinklers[1] = PIN_S2;
+	sprinklers[2] = PIN_S3;
+	sprinklers[3] = PIN_S4;
+
+	//    Blynk.virtualWrite(V1, wateringTimeSliderValue);
+	//    Blynk.virtualWrite(V1, 0);
+	unsigned long second = 1000L;
+	unsigned long minute = 60 * second;
+	unsigned long hour = 60 * minute;
+
+	unsigned long checkTankInterval = 20 * minute;
+	unsigned long wateringRoutineInterval = 1 * minute;
+
+	timer.setInterval(checkTankInterval, checkWaterTank);
+	timer.setInterval(wateringRoutineInterval, wateringRoutine);
+
+	// Setup a function to be called every second
+	//  timer.setInterval(1000L, sendSeconds);
+	// Setup a function to be called every second
+	//  timer.setInterval(1000L, sendMillis);
+
+	while (Blynk.connect() == false) {
+		// Wait until connected
+	}
+
+	resetWateringRoutine();
+
+	lcd.clear(); //Use it to clear the LCD Widget
+	lcd.print(0, 0, "                ");
+	lcd.print(0, 0, "READY");
+	//  lcd.print(0, 0, ""); // use: (position X: 0-15, position Y: 0-1, "Message you want to print")
+	//  lcd.print(0, 1, "                ");
+	value_water = analogRead(PIN_WATER_SENSOR);
+	//tank not full
+	//  lcd.print(0, 1, "                ");
+	if (value_water > 800) {
+		digitalWrite(PIN_WELL_PUMP, LOW);
+		lcd.print(0, 1, "Filling tank...");
+		tankFull = false;
+		//    tankNeedsFilling=true;
+	} else {
+		lcd.print(0, 1, "Tank full");
+		tankFull = true;
+	}
+}
+
+void loop() {
+	//tank is full
+	if (analogRead(PIN_WATER_SENSOR) < 800 && tankFull == false) {
+		lcd.print(0, 1, "                ");
+		lcd.print(0, 1, "Tank full");
+		digitalWrite(PIN_WELL_PUMP, HIGH);
+		tankFull = true;
+		//    timerWell.restartTimer(1);
+	}
+	Blynk.run();
+	timer.run();
+	//  timerWell.run();
 }
